0667c3637c4fb1f493a89ceaa64e033430f822bb
Bolot Kerimbaev <bolot@bignerdranch.com>
2014-01-21 17:41:35 +0100

    simple transition

--- TransitionExample/src/main/java/com/bignerdranch/android/transitionexample/TransitionFragment.java
+++ TransitionExample/src/main/java/com/bignerdranch/android/transitionexample/TransitionFragment.java
@@ -2,9 +2,12 @@ package com.bignerdranch.android.transitionexample;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,6 +21,14 @@ public class TransitionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_transition_scene_1, container, false);
+        final Scene scene = Scene.getSceneForLayout(container, R.layout.fragment_transition_scene_2, getActivity());
+        Button goButton = (Button)rootView.findViewById(R.id.goButton);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.go(scene);
+            }
+        });
         return rootView;
     }
 }
