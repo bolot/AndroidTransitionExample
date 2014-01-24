cc4087352b28a8b590076953da6d2246df2383be
Bolot Kerimbaev <bolot@bignerdranch.com>
2014-01-24 21:53:30 +0100

    load the transition manager from the XML file

--- TransitionExample/src/main/java/com/bignerdranch/android/transitionexample/TransitionActivity.java
+++ TransitionExample/src/main/java/com/bignerdranch/android/transitionexample/TransitionActivity.java
@@ -2,10 +2,18 @@ package com.bignerdranch.android.transitionexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class TransitionActivity extends Activity {
+    private TransitionManager mTransitionManager;
+    private Scene mScene1;
+    private Scene mScene2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +25,20 @@ public class TransitionActivity extends Activity {
                     .add(R.id.container, new TransitionFragment())
                     .commit();
         }
+        ViewGroup container = (ViewGroup)findViewById(R.id.container);
+        TransitionInflater transitionInflater = TransitionInflater.from(this);
+        mTransitionManager = transitionInflater.inflateTransitionManager(R.transition.transition_manager, container);
+        mScene1 = Scene.getSceneForLayout(container, R.layout.fragment_transition_scene_1, this);
+        mScene2 = Scene.getSceneForLayout(container, R.layout.fragment_transition_scene_2, this);
     }
 
+    public void goToScene1(View view) {
+        mTransitionManager.transitionTo(mScene1);
+    }
+
+    public void goToScene2(View view) {
+        mTransitionManager.transitionTo(mScene2);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- TransitionExample/src/main/java/com/bignerdranch/android/transitionexample/TransitionFragment.java
+++ TransitionExample/src/main/java/com/bignerdranch/android/transitionexample/TransitionFragment.java
@@ -2,14 +2,9 @@ package com.bignerdranch.android.transitionexample;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.transition.Scene;
-import android.transition.Transition;
-import android.transition.TransitionInflater;
-import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -23,29 +18,7 @@ public class TransitionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_transition_scene_1, container, false);
-        View secondView = inflater.inflate(R.layout.fragment_transition_scene_2, container, false);
-        final Scene scene = new Scene(container, (ViewGroup)secondView);
-        Button goButton = (Button)rootView.findViewById(R.id.goButton);
-        goButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToScene(scene);
-            }
-        });
-        final Scene originalScene = new Scene(container, (ViewGroup)rootView);
-        Button goBackButton = (Button)secondView.findViewById(R.id.goButton);
-        goBackButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToScene(originalScene);
-            }
-        });
         return rootView;
     }
 
-    private void goToScene(Scene scene) {
-        TransitionInflater transitionInflater = TransitionInflater.from(getActivity());
-        Transition transition = transitionInflater.inflateTransition(R.transition.slow_auto_transition);
-        TransitionManager.go(scene, transition);
-    }
 }
